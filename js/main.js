c763c92d27e9bb4975b5f1fbbc11623a6c7c19f6
Elias Simon <riesyeti@outlook.de>
2020-01-08 18:35:01 +0100

    test changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // most of this is totally not stolen from w3 i swear
-console.log("js is working")
+console.log("js is working!!")
 
 // sticky header currently not working for navbar but floating navbar might be a better choice anyway, I will just leave this here for future reference
 window.onscroll = function() {stickyheader()};
