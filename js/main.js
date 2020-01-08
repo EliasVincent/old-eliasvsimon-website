6e9189f6a4a181d14c83f78441b9366ab8d85150
Elias Simon <riesyeti@outlook.de>
2020-01-08 18:22:53 +0100

    new file:   contact.html 	new file:   css/style.css 	new file:   downloads.html 	new file:   downloads/passion.zip 	new file:   images/downloads-showcase.png 	new file:   images/navbar.png 	new file:   images/navbar.png.alt 	new file:   images/passion.jpg 	new file:   images/showcase.png 	new file:   index.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// most of this is totally not stolen from w3 i swear
+console.log("js is working!")
+
+// sticky header currently not working for navbar but floating navbar might be a better choice anyway, I will just leave this here for future reference
+window.onscroll = function() {stickyheader()};
+let header = document.getElementById("main-header");
+let sticky = header.offsetTop;
+
+function stickyheader() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  } 
+
+
+// sticky navbar 
+window.onscroll = function() {stickynavbar()};
+
+let navbar = document.getElementById("navbar");
+let sticky2 = navbar.offsetTop;
+
+
+function stickynavbar() {
+    // if the page y axis is greater than stick (which is at offset Top)
+    if (window.pageYOffset > sticky) {
+        // variable navbar which is got the ID navbar, adds the class sticky2 to itself, which triggers the CSS of sticky 2 being sticky at the top
+        navbar.classList.add("sticky2");
+      } else {
+        navbar.classList.remove("sticky2");
+      }
+}
+
